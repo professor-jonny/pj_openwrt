f0b3f2bc824f4955bc15a57836ebebd51394f5ef
professor-jonny <professor_jonny@hotmail.com>
2024-07-20 00:19:48 +1200

    Qualcommax: IPQ807x: Add support for TP-Link Deco X80-5G
    The TP-link Deco X80-5G is a AX WIFI router with a single 1G (LAN) and 1 2.5G (WAN) port with a built in Quectel 5G modem.
    
    Specifications:
    
    Architecture	ARMv8-A (aarch64 Cortex A53, 4 cores)
    Vendor	Qualcomm
    Bootloader	U-Boot (2016)
    System-On-Chip	Qualcomm IPQ8074A (Networking Pro 1210 platform)
    CPU/Speed	2.2GHZ
    Flash-Chip	ESMT F59D1G81MB-45TI 1G-bit NAND
    Flash MB	128MB (1Gbit)
    RAM-chip	2* ESMT M15T4G16256A-DEBG2G 2Gb DDR3L (32bit)
    RAM MB	512MB (4Gbit)
    WLan System-On-Chip (5g)	Qualcomm QCN5054
    WLan Front end modules (5g)	8* Qorvo QPF4588
    WLAN (5G)	A/N/AC/AX
    WLAN Antenna (int)	8* (4 shared with 2.5G)
    WLan System-On-Chip (2.4G)	Qualcomm QCN5024
    WLan Front end modules (2.4G)	4* Skyworks SKY85340-11
    WLAN (2.4G)	B/G/N/AX
    WLAN Antenna (int)	4* (shared with 5g)
    Eithernet-phy (1Gbit)	Qualcomm (Atheros) AR8033-AL1A
    Ethernet (1Gbit)	1*
    Eithernet-phy (2.5Gbit)	Qualcomm QCA8081
    Ethernet (2.5Gbit)	1*
    Switch	Embedded in the SOC?
    Serial	1.8v 8n1 (testpoints)
    Modem System-On-Chip	Qualcomm 5G RG50xQ (Snapdragon X55 platform)
    5G modem capability	5000Mbps down 900Mbps up
    5G modem bands	N1/N3/N5/N7/N8/N20/N28/N38/N40/N41/N77/N78/N79
    4G modem capability	4.5G LTE-Advanced Pro (Cat20 down 2Gbps) (Cat18 up 200Mbps)
    4G modem FDD bands	B1/B3/B5/B7/B8/B18/B19/B20/B26/B28
    4G modem TDD bands	B34/B38/B39/B40/B41/B42/B43
    Modem antenna (int)	8* (5G/4G use)
    Modem antenna (ext)	2* SMA connectors (5G/4G use)
    SIM type	1* Nano SIM
    Telephony	1* POTS RJ11
    (see wiki for more info:)
    https://openwrt.org/inbox/toh/tp-link/x80-5g_v1
    
            Notes:
    Installation Instructions:
    
    Between antennas 7 and 8 on the PCB with the RGB harness there are three test pads labelled TP1 TP2 and TP3 connections are as follows:
      |TP2|-- RX
      |TP1|-- TX
      |TP3|-- GND
    
    RX requires an external pullup to operate somewhere around 5 K ohm but your luck may vary the Uart is 1.8v
    
    Set a static IP and set up a tftpserver and terminal.
    power the router and quickly type the magic string “tpl” and press enter to break into u-boot
    in the shell set the environment variables to enable tftpbooting
    setenv ipaddr (routerIP)
    setenv serverip (server IP)
    load you initramfs:
    tftpboot 0x44000000 (serverIP):openwrt-qualcommax-ipq807x-tplink_x80-5g-initramfs-uImage.itb
    boot your initramfs
    bootm
    upload your factory image to /tmp
    format and install the factory image:
    ubiformat /dev/mtd12 -y -f /tmp/openwrt-qualcommax-ipq807x-tplink_x80-5g-squashfs-factory.ubi
    
    Add script to mount partition for MAC recovery,
    Add script to pull mac from factory data partition.
    Add script to patch LAN MAC
    Add script  to patch ART with MAC.
    Add script to control RGB LED as no way exists to control this within the standards of OpenWrt.
    
    GPIO fan tables work like the  stock device with three fan maps high active idle with a top fan speed of 6800 RPM.
    
    High temp 70 deg speeds 6-8
    Active temp 50 deg speeds 2-5
    low temp 25 degree speed 0-1
    
    Signed-off-by: Jonathan Brophy <professor_jonny@hotmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -1,4 +1,4 @@
-PART_NAME=firmware
+ PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
 RAMFS_COPY_BIN='fw_printenv fw_setenv head'
@@ -262,6 +262,7 @@ platform_do_upgrade() {
 		CI_DATAPART="rootfs_data"
 		emmc_do_upgrade "$1"
 		;;
+	tplink,deco-x80-5g|\
 	tplink,eap620hd-v1|\
 	tplink,eap660hd-v1)
 		tplink_do_upgrade "$1"
